bc48f417bc7e69c5fa5799435f04e4db8344cb48
TitanX1503 <60981537+TitanX1503@users.noreply.github.com>
2020-04-29 16:55:04 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Console_Menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+
+            ¡Bienvenido a la nevería!
+
+            1) Tipo de cono
+            2) Sabor de nieve
+            3) Sencillo o doble
+            4) Con o sin chocolate
+            5) Toppings
+
+            6) Pagar
+
+            9) Salir
+
+            Selecciona una opción:
+            > 1
+            [enter]
+
+            */
+
+            Menu menu = new Menu();
+            menu.Display();
+            
+        }
+    }
+}
